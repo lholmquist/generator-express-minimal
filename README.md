e8f56fe1cffab939a504c4c8c7ec87d46e0a7df0
Lucas Holmquist <lholmqui@redhat.com>
2014-02-06 15:21:24 -0500

    first

---
+++ README.md
@@ -0,0 +1,27 @@
+# generator-express-simple [![Build Status](https://secure.travis-ci.org/lholmquist/generator-express-simple.png?branch=master)](https://travis-ci.org/lholmquist/generator-express-simple)
+
+> [Yeoman](http://yeoman.io) generator
+
+
+To install generator-express-simple from npm, run:
+
+```
+$ npm install -g generator-express-simple
+```
+
+Finally, initiate the generator:
+
+```
+$ yo express-simple
+```
+
+### Getting To Know Yeoman
+
+Yeoman has a heart of gold. He's a person with feelings and opinions, but he's very easy to work with. If you think he's too opinionated, he can be easily convinced.
+
+If you'd like to get to know Yeoman better and meet some of his friends, [Grunt](http://gruntjs.com) and [Bower](http://bower.io), check out the complete [Getting Started Guide](https://github.com/yeoman/yeoman/wiki/Getting-Started).
+
+
+## License
+
+MIT
---
+++ app/index.js
@@ -0,0 +1,62 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+
+var ExpressSimpleGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.npmInstall();
+      }
+    });
+  },
+
+  // askFor: function () {
+  //   var done = this.async();
+
+  //   // have Yeoman greet the user
+  //   console.log(this.yeoman);
+
+  //   // replace it with a short and sweet description of your generator
+  //   console.log(chalk.magenta('You\'re using the fantastic ExpressSimple generator.'));
+
+  //   var prompts = [{
+  //     type: 'confirm',
+  //     name: 'someOption',
+  //     message: 'Would you like to enable this option?',
+  //     default: true
+  //   }];
+
+  //   this.prompt(prompts, function (props) {
+  //     this.someOption = props.someOption;
+
+  //     done();
+  //   }.bind(this));
+  // },
+
+  app: function () {
+    this.mkdir('public');
+    this.mkdir('public/css');
+    this.mkdir('public/js');
+
+    this.copy('server.js', 'server.js');
+    this.copy('public/index.html', 'public/index.html');
+    this.copy('public/css/style.css', 'public/css/style.css');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+  },
+
+  projectfiles: function () {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+    this.copy('bowerrc', '.bowerrc');
+  }
+});
+
+module.exports = ExpressSimpleGenerator;
